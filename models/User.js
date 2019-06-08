ab2a76bf6a1788575033c88006d24dca13e1031b
aayushraj07 <aayush.97raj@gmail.com>
2019-06-08 11:04:26 +0530

    Initial express server with route files

---
+++ models/User.js
@@ -0,0 +1,33 @@
+
+const mongooese = require ('mongoose');
+const schema = mongoose.Schema;
+
+//Create Schema
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+});
+
+module.exports = Users = mongooese.model('')
